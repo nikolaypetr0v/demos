141714ef77757042e24e439711fe229cda9a2d00
Nikolay Petrov <nikolai.k.petrov@abv.bg>
2024-03-16 14:02:48 +0200

    B2 rebase code added.

--- src/main/java/SecondBranch.java
+++ src/main/java/SecondBranch.java
@@ -3,5 +3,8 @@ package main.java;
 public class SecondBranch {
     public static void main(String[] args) {
         System.out.println("Second Branch.");
+        System.out.println("Stash msg 1.");
+
+        System.out.println("Rebase");
     }
 }
